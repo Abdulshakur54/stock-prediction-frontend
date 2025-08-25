ac1ba6494476dea81e744498cd814f8a964ee3ea
Abdulshakur Muhammad <33991143+Abdulshakur54@users.noreply.github.com>
2025-08-26 00:18:42 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   const location = useLocation();
   const handleLogout = async () => {
     try {
-      await axiosInstance.post("logout/");
+      // await axiosInstance.post("logout/");
       localStorage.removeItem("loggedIn");
       localStorage.removeItem("csrfToken");
       setIsLoggedIn(false);
