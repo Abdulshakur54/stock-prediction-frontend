d6fadccd3cc60c7eddd1b3436c6d2332fd3be169
Abdulshakur <mabdulshakur54@gmail.com>
2025-10-18 16:42:54 +0100

    made technical changes to vite.config.js to aid file referencing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ correct for Netlify or any root-hosted app
+  publicDir: "public",
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
+
